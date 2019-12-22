b589aadce61b3da64c67118c2a016f869f53bfdb
Gries, Matt <Matt.Gries@freedompay.com>
2019-12-22 14:39:56 -0500

    Basic frontend setup

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
     return (
         <Switch>
-            <Route path="/" exact component={Home} />
-
+            <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+            <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+            <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
             { /* Finally, catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
