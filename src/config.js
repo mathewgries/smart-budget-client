1c43e0afe4e242f395e0d8de270033f847b30bb3
Gries, Matt <Matt.Gries@freedompay.com>
2019-12-22 14:02:51 -0500

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://1yt69x59a7.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_fYW63djG8",
+        APP_CLIENT_ID: "5ek6hrs9286gcsghknhc5cn5b2",
+        IDENTITY_POOL_ID: "us-east-1:8793d470-ec89-414c-8215-53114b13593d"
+    }
+};
